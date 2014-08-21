54ea9f21b9dc56aeb7c89625d89315b94c177c74
fatsea <594086561@qq.com>
2014-08-21 16:28:06 +0800

    2014/8/21/16/27
    webqq完成

--- 高中恒/webqq/js/qq.js
+++ 高中恒/webqq/js/qq.js
@@ -60,56 +60,130 @@
   	  	$(this).parent().find(".qqmiddlebuttomnum2").css("background-image",'url("css/images/tab_icon_contact.png")')
   	  	$(this).parent().find(".qqmiddlebuttomnum3").css("background-image",'url("css/images/tab_icon_plugin.png")')
   	 });
+        $(".footerbuttom1").click(function(){
+          
+        bianhuan();
+     });
+        $(".footerbuttom2").click(function(){
+        bianhuan2();
+     });
+        $(".shezhilist3").click(function(){
+          $(this).parent().parent().parent().parent().parent().parent().find(".panelNotifySetting").show();
+     });
+        $(".panelSetHeaderButton").click(function(){
+          $(this).parent().parent().hide();
+     });
 
 
-       $(".friend").click(function(){
-    var friendID = $(this).attr("friendID");
-    var chatDivID = "chat" + friendID;
-    if ($(this).attr("isOpen") == "true") {
-      //将已经打开的窗口置顶
-      $(".qqChat").css("z-index","15");
-      $("#" + chatDivID).css("z-index","16");
-      return;
-    }
-    $(this).attr("isOpen","true");
-    
-    var friendName = $(this).attr("friendName");
-    var html ="";
-    html += '   <div id="' + chatDivID + '" class="Chat">';
-    html += '     <div class="chatTitle">';
-    html += '       <div class="chattitle1"><div class="chattitle1div"><div class="chattitle1divxiala"></div></div></div>';
-    html += '       <div class="chatperson">' + friendName +'</div>';
-    html += '       <div friendID = "friend'+friendID+'" chatDivID="' + chatDivID + '" class="chattitle3"><div class="chattitle3div"><span class="chattitle3divspan">关闭</span></div></div>';
-    html += '     </div>';
-    html += '     <div class="chatHistory"></div>';
-    html += '     <div class="chatControl"></div>';
-    html += '   </div>';
 
-      
-      
+        $(document).on("click", ".chatshuruchatsendbuttom",function(){
+            var chatContentHtml ="";
 
+            var MyWords = $(this).parent().parent().find(".chatshuruchatkuang").val();
+            chatContentHtml +='       <div class="chatDesContentA">';
 
-        <div class="chatcontent"></div>
-    <div class="chatshuru">
-      <div class="chatshurubq">
-        <div class="chatshurubqpic"></div>
-      </div>
-      <textarea class="chatshuruchatkuang"></textarea>
-      <div class="chatshuruchatsend">
-        <span class="chatshuruchatsendbuttom">发送</span>
-      </div>
-    </div>
-  </div>
+            chatContentHtml +='         <div class="cDAHeader"></div>';
+            chatContentHtml +='         <div class="cDALeft">';
+            chatContentHtml +='           <span class="cDAMyNicName">胖大海</span>';
+            chatContentHtml +='           <div class="cDAMyWords">'+MyWords+'</div>';
+            chatContentHtml +='         </div>';                
+            chatContentHtml +='       </div>';
+            
+            $(this).parent().parent().parent().find(".chatcontent").append(chatContentHtml);
+            var curContent = $(this).parent().parent().parent().find(".chatcontent");
+            
 
+            curContent.animate({"scrollTop":"1000000x"},0);
+            $(this).parent().parent().find(".chatshuruchatkuang").val("");
+          });
 
-    $("body").append(html);
+        tcliaotian();
 
  });
 
+  function tcliaotian()
+  
+     {
+         $(".friend").click(function(){
+          
+          var talkid=$(this).attr("talkid");
+          var cAname=$(this).attr("cafName");
+          var isappear = $(this).attr("isappear");
+           if(isappear=="yes"){
+              $("#"+talkid).show();
+              $(".chat").css("z-index","10");
+              $("#"+talkid).css("z-index","12");
+            return;
+           }
+          
+         var chatHtml = '';
+    
+         chatHtml+=' <!--聊天框-->';
+         chatHtml+='<div id="'+ talkid +'" class="chat">';
+         chatHtml+='    <div class="chattitle">';
+         chatHtml+='        <div class="chattitle1">';
+         chatHtml+='            <div class="chattitle1div">';
+         chatHtml+='                <div class="chattitle1divxiala"></div>';
+         chatHtml+='            </div>';
+         chatHtml+='        </div>';
+         chatHtml+='        <div class="chatperson">'+ cAname +'</div>';
+         chatHtml+='        <div class="chattitle3">';
+         chatHtml+='            <div class="chattitle3div">';
+         chatHtml+='                <span class="chattitle3divspan">关闭</span>';
+         chatHtml+='            </div>';
+         chatHtml+='        </div>';
+         chatHtml+='    </div>';
+         chatHtml+='    <div class="chatcontent"></div>';
+         chatHtml+='    <div class="chatshuru">';
+         chatHtml+='        <div class="chatshurubq">';
+         chatHtml+='            <div class="chatshurubqpic"></div>';
+         chatHtml+='        </div>';
+         chatHtml+='        <textarea class="chatshuruchatkuang"></textarea>';
+         chatHtml+='        <div class="chatshuruchatsend">';
+         chatHtml+='            <span class="chatshuruchatsendbuttom">发送</span>';
+         chatHtml+='        </div>';
+         chatHtml+='    </div>';
+         
+        
 
+         $("body").append(chatHtml);
+         $(this).attr("isappear","yes");
+         $(".chat").css("z-index","10");
+         $("#"+talkid).css("z-index","12");
 
+         var ranleft= (Math.random()+5)*100;
+         var rantop= Math.random()*150;
+          $("#"+talkid).css({
+            "left":ranleft+"px",
+            "top":rantop+"px"
+          });
 
+        $(".chattitle3div").click(function(){
+              $(this).parent().parent().parent().hide();
+        });
+        
 
+     });
+  };
+  
+  var index=2;
+  function bianhuan(){
+
+      $("#webqqall").css("background-image",'url("css/images/'+index+'.jpg")');
+      index++;
+      if (index>3) {
+        index=1;
+      };
+  }
+
+  var m=3
+  function bianhuan2(){
+      $("#webqqall").css("background-image",'url("css/images/'+ m +'.jpg")');
+      m--;
+      if (m<1) {
+        m=3;
+      };
+  } 
 
 
 
